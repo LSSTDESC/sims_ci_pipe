e289e1fe863a7d721e20bb51f5875a51e940fedb
James Chiang <jchiang87@gmail.com>
2019-12-02 21:26:18 -0800

    add FWHMgeom and FWHMeff functions

--- python/desc/sims_ci_pipe/opsim_db_interface.py
+++ python/desc/sims_ci_pipe/opsim_db_interface.py
@@ -11,6 +11,79 @@ except ImportError as eobj:
 __all__ = ['OpSimDb']
 
 
+def airmass(altitude):
+    """
+    Function to compute the airmass from altitude using equation 3
+    of Krisciunas and Schaefer 1991.
+
+    Parameters
+    ----------
+    altitude: float
+        Altitude of pointing direction in degrees.
+
+    Returns
+    -------
+    float: the airmass in units of sea-level airmass at the zenith.
+    """
+    altRad = np.radians(altitude)
+    return 1.0/np.sqrt(1.0 - 0.96*(np.sin(0.5*np.pi - altRad))**2)
+
+
+def FWHMeff(rawSeeing, band, altitude):
+    """
+    Compute the effective FWHM for a single Gaussian describing the PSF.
+
+    Parameters
+    ----------
+    rawSeeing: float
+        The "ideal" seeing in arcsec at zenith and at 500 nm.
+        reference: LSST Document-20160
+    band: str
+        The LSST ugrizy band.
+    altitude: float
+        The altitude in degrees of the pointing.
+
+    Returns
+    -------
+    float: Effective FWHM in arcsec.
+    """
+    X = airmass(altitude)
+
+    # Find the effective wavelength for the band.
+    wl = dict(u=365.49, g=480.03, r=622.20, i=754.06, z=868.21, y=991.66)[band]
+
+    # Compute the atmospheric contribution.
+    FWHMatm = rawSeeing*(wl/500)**(-0.3)*X**(0.6)
+
+    # The worst case instrument contribution (see LSE-30).
+    FWHMsys = 0.4*X**(0.6)
+
+    # From LSST Document-20160, p. 8.
+    return 1.16*np.sqrt(FWHMsys**2 + 1.04*FWHMatm**2)
+
+
+def FWHMgeom(rawSeeing, band, altitude):
+    """
+    FWHM of the "combined PSF".  This is FWHMtot from
+    LSST Document-20160, p. 8.
+
+    Parameters
+    ----------
+    rawSeeing: float
+        The "ideal" seeing in arcsec at zenith and at 500 nm.
+        reference: LSST Document-20160
+    band: str
+        The LSST ugrizy band.
+    altitude: float
+        The altitude in degrees of the pointing.
+
+    Returns
+    -------
+    float: FWHM of the combined PSF in arcsec.
+    """
+    return 0.822*FWHMeff(rawSeeing, band, altitude) + 0.052
+
+
 class OpSimDb:
     def __init__(self, opsim_db_file=None):
         if opsim_db_file is None:
@@ -26,11 +99,9 @@ class OpSimDb:
                     obsHistID={visit} limit 1'''
         row = self.query(query).iloc[0]
         row['band'] = row['filter']
-        if desc_imsim is not None:
-            row['FWHMgeom'] = desc_imsim.FWHMgeom(row.rawSeeing, row.band,
-                                                  np.degrees(row.altitude))
-            row['FWHMeff'] = desc_imsim.FWHMeff(row.rawSeeing, row.band,
-                                                np.degrees(row.altitude))
+        altitude = np.degrees(row.altitude)
+        row['FWHMgeom'] = FWHMgeom(row.rawSeeing, row.band, altitude)
+        row['FWHMeff'] = FWHMeff(row.rawSeeing, row.band, altitude)
         return row
 
     def query(self, query):
