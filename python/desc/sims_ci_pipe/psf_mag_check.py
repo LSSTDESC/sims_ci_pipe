edaf4de6d17e41dbe5b2f08250a132861afdb272
Jim Chiang <jchiang@slac.stanford.edu>
2019-12-18 09:04:53 -0800

    use median to estimate delta_mag offset

--- python/desc/sims_ci_pipe/psf_mag_check.py
+++ python/desc/sims_ci_pipe/psf_mag_check.py
@@ -5,7 +5,6 @@ for biases in photometry.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn.mixture
 import lsst.daf.persistence as dp
 from .ellipticity_distributions import get_point_sources
 
@@ -46,36 +45,8 @@ def get_psf_calib_mags(butler, visit):
     return pd.DataFrame(data=dict(psf_mag=psf_mags, calib_mag=calib_mags))
 
 
-def fit_gmm(x, n_components=2):
-    """
-    Fit a multicomponent Gaussian mixture model to the distribution
-    and return the fit results for each component, ordered by weight,
-    descending.
-
-    Parameters
-    ----------
-    x: list-like
-        The values for which the distribution will be modeled.
-    n_components: int [2]
-        Number of Gaussian components to include in the model.
-
-    Returns
-    -------
-    n_components tuple of (weight, mean, sigma) for each component
-    """
-    gmm = sklearn.mixture.GaussianMixture(n_components=n_components,
-                                          warm_start=True)
-    gmm = gmm.fit(x[:, np.newaxis])
-    gmm = gmm.fit(x[:, np.newaxis])
-    gmm = gmm.fit(x[:, np.newaxis])
-    component_pars = []
-    for weight, mean, covar in zip(gmm.weights_.ravel(), gmm.means_.ravel(),
-                                   gmm.covariances_.ravel()):
-        component_pars.append((weight, mean, np.sqrt(covar)))
-    return sorted(component_pars, key=lambda x: x[0], reverse=True)
-
-
-def psf_mag_check(repo, visit, outfile=None, figsize=(6, 4)):
+def psf_mag_check(repo, visit, outfile=None, dmag_range=(-0.05, 0.05),
+                  figsize=(6, 4)):
     """
     Plot distribution of  delta_mag = psf_mag - calib_mag values, and
     return estimate of the delta_mag peak location.
@@ -87,7 +58,12 @@ def psf_mag_check(repo, visit, outfile=None, figsize=(6, 4)):
     visit: int
         Visit number to consider.
     outfile: str [None]
-        Output filename for plot. If None, then 'delta_mag_{visit
+        Output filename for plot. If None, then use
+        'delta_mag_v{visit}-{band}.png'.
+    dmag_range: (float, float) [(-0.05, 0.05)]
+        Magnitude range to use for plotting and median estimation.
+    fig_size: (float, float) [(6, 4)]
+        matplotlib figure dimensions in inches.
 
     Returns
     -------
@@ -98,12 +74,13 @@ def psf_mag_check(repo, visit, outfile=None, figsize=(6, 4)):
     df = get_psf_calib_mags(butler, visit)
     if len(df) == 0:
         return None
-    delta_mag = df['psf_mag'] - df['calib_mag']
-    _, dmag_peak, _ = fit_gmm(delta_mag, n_components=2)[0]
-    plt.hist(delta_mag, range=(-0.05, 0.05), bins=100, histtype='step')
+    delta_mag = (df['psf_mag'] - df['calib_mag']).to_numpy()
+    index = np.where((dmag_range[0] < delta_mag) & (delta_mag < dmag_range[1]))
+    dmag_median = np.median(delta_mag[index])
+    plt.hist(delta_mag, range=dmag_range, bins=100, histtype='step')
     plt.axvline(0, linestyle=':')
-    plt.axvline(dmag_peak, linestyle='--')
-    plt.annotate(f'peak: {dmag_peak*1000:.2f} mmag', (0.1, 0.95),
+    plt.axvline(dmag_median, linestyle='--')
+    plt.annotate(f'median: {dmag_median*1000:.2f} mmag', (0.1, 0.95),
                  xycoords='axes fraction')
     plt.xlabel('psf_mag - calib_mag')
     plt.legend(fontsize='x-small')
@@ -113,4 +90,4 @@ def psf_mag_check(repo, visit, outfile=None, figsize=(6, 4)):
         outfile = f'delta_mag_v{visit}-{band}.png'
     plt.savefig(outfile)
 
-    return dmag_peak
+    return dmag_median
