d9973aaed172aea5434587888e2f365aab2b43f4
Alex Drlica-Wagner <kadrlica@fnal.gov>
2020-07-15 08:52:43 -0700

    pass xy offset to x and y offsets

--- bin.src/make_star_grid_instcat.py
+++ bin.src/make_star_grid_instcat.py
@@ -15,5 +15,6 @@ args = parser.parse_args()
 
 grid_instcat = scp.make_star_grid_instcat(args.instcat,
                                           detectors=args.detectors,
-                                          max_xy_offset=args.max_xy_offset)
+                                          max_x_offset=args.max_xy_offset,
+                                          max_y_offset=args.max_xy_offset)
 scp.make_reference_catalog(grid_instcat)
